c2da3dea242accedbf0493f2c85b6c2316d53db6
mahdi-laptop <imani.mahdi@gmail.com>
2022-02-02 23:58:25 +0330

    feat: test mock data

--- internal/service/segment.go
+++ internal/service/segment.go
@@ -23,13 +23,28 @@ func (u *segmentService) StoreUserSegmantation(ctx context.Context, pb_request *
 }
 
 func (u *segmentService) ShowUserInSegmant(ctx context.Context, pb_request *pb.SegmantRequest) (*pb.SegmantResponse, error) {
-	return &pb.SegmantResponse{
-		Users: []*pb.User{
-			{
-				User:    "12",
-				Segmant: "Football",
-			},
+	result := []*pb.User{}
+	db := []*pb.User{
+		{
+			User:    "1",
+			Segmant: "Football",
 		},
+		{
+			User:    "2",
+			Segmant: "Football",
+		},
+		{
+			User:    "3",
+			Segmant: "Vollybal",
+		},
+	}
+	for _, value := range db {
+		if value.Segmant == pb_request.Segmant {
+			result = append(result, value)
+		}
+	}
+	return &pb.SegmantResponse{
+		Users:   result,
 		Success: "true",
 	}, nil
 }
